2a6eb5efd7b6f8525f206a8a587b1fae2e5221a9
Andrew Lloyd <andrew85.lloyd@gmail.com>
2021-11-22 20:44:53 -0800

    update

--- main.go
+++ main.go
@@ -3,11 +3,20 @@ package main
 import (
 	"fmt"
 	"time"
+
+	"github.com/robfig/cron/v3"
 )
 
 func main() {
 
 	fmt.Println("Running application now")
+	var cronLib *cron.Cron
+	cronLib = cron.New()
+	cronLib.AddFunc("@every 15s", func() {
+		fmt.Println("")
+	})
+	cronLib.Start()
+
 	go forever()
 	select {} // block forever
 }
