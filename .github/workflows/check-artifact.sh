df0d4a929faa2b73e543351dec26afcc03be9beb
Andrew Lloyd <andrew85.lloyd@gmail.com>
2022-02-12 21:00:08 -0800

    Script update

---
+++ .github/workflows/check-artifact.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+retries=0
+found=false
+while [[ ${found} == false ]]; do
+  artifacts=$(curl -s -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/andrewmarklloyd/pi-test/actions/artifacts | grep app_${{ github.sha }})
+  if [[ ! -z ${artifacts} ]]; then
+    found=true
+  else
+    echo "Artifact not found, retrying in 5 seconds"
+    retries=$((retries+1))
+    sleep 5
+  fi
+  if (( ${retries} >= 5 )); then
+    echo "Max retries exceeded, failing now"
+    exit 1
+  fi
+done
