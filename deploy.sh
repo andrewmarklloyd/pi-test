20d3c0feec41c96965cbdbb3e9ccf771bdfc0c70
Andrew Lloyd <andrew85.lloyd@gmail.com>
2022-03-09 20:46:34 -0800

    Fix script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-response=$(curl -s -X POST -H "api-key: ${PI_APP_DEPLOYER_API_KEY}" -d '{"sha":"${{ github.sha }}","repository":"${{ github.repository }}","name":"app_${{ github.sha }}","manifest_name":"pi-test-arm"}' https://pi-app-deployer.herokuapp.com/push)
+response=$(curl -s -X POST -H "api-key: ${PI_APP_DEPLOYER_API_KEY}" -d "{\"sha\":\"${{ github.sha }}\",\"repository\":\"${GITHUB_REPOSITORY}\",\"name\":\"app_${GITHUB_SHA}\",\"manifest_name\":\"pi-test-arm\"}" https://pi-app-deployer.herokuapp.com/push)
 echo ${response}
 if [[ $(echo ${response} | jq -r '.status') != "success" ]]; then
   exit 1
