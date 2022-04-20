4d70970d10d733c661cfa84f29e47218a3a83c6f
Andrew Lloyd <andrew85.lloyd@gmail.com>
2022-04-20 11:42:33 -0700

    Rename var

--- scripts/get-deployer-vars.sh
+++ scripts/get-deployer-vars.sh
@@ -4,7 +4,7 @@
 line=$(tail -n 1 test/integration-trigger.txt)
 arr=($(echo $line | tr ";" "\n"))
 host=${arr[1]}
-echo "::set-output name=DEPLOYER_HOST::$(echo ${host})"
+echo "::set-output name=DEPLOYER_APP::$(echo ${host})"
 
 if [[ ${host} == "pi-app-deployer-staging" ]]; then
   echo "::set-output name=PI_APP_DEPLOYER_API_KEY::$(echo ${PI_APP_DEPLOYER_API_KEY_STAGING})"
